11ce52f52ba4c67558764e150b1b7d5234119ebd
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2021-07-31 13:40:47 +0200

    Updated code

--- Advanced/Windi CSS/main.js
+++ Advanced/Windi CSS/main.js
@@ -4,11 +4,17 @@ import 'virtual:windi.css'
 import './style.css'
 import { createApp } from 'vue'
 
+import CurrentBreakpoint from '@/components/current-breakpoint.vue'
+
 const App = {
   data () {
     return {
       message: 'Hello World'
     }
+  },
+
+  components: {
+    CurrentBreakpoint
   }
 }
 createApp(App).mount('#app')
